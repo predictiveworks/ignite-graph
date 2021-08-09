8a82966564216e6548784cec352b0743e0bbf627
Predictive Works <info@predictiveworks.eu>
2021-08-09 09:27:05 +0200

    RDD read & write support updated.

---
+++ src/main/scala/de/kp/works/ignite/rdd/RDDWriter.scala
@@ -0,0 +1,43 @@
+package de.kp.works.ignite.rdd
+/*
+ * Copyright (c) 20129 - 2021 Dr. Krusche & Partner PartG. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * @author Stefan Krusche, Dr. Krusche & Partner PartG
+ *
+ */
+
+import org.apache.ignite.binary.BinaryObject
+import org.apache.ignite.configuration.CacheConfiguration
+import org.apache.ignite.spark.IgniteContext
+import org.apache.spark.sql.{DataFrame, Row}
+
+abstract class RDDWriter(
+  ic:IgniteContext,
+  cfg:CacheConfiguration[String,BinaryObject]) {
+
+  def write(dataframe:DataFrame,
+            table:String, f: (Row, IgniteContext) ⇒ (String, BinaryObject),
+            overwrite:Boolean = true,
+            keepBinary:Boolean = false):Unit = {
+    /*
+     * IgniteBatchRDD is an application controlled variant
+     * of Apache Ignite's [IgniteRDD] that extends this class
+     * with a batch receiver for large-scale processing.
+     */
+    val cache = new IgniteBatchRDD(ic,table,cfg,keepBinary)
+    cache.savePairs(dataframe.rdd, f, overwrite, keepBinary)
+
+  }
+}
---
+++ src/main/scala/de/kp/works/ignite/rdd/README.md
@@ -0,0 +1,7 @@
+# RDD Read & Write Support
+
+This package supports read & write operations for edges and vertices and perfectly
+fits into a DataFrame- or GraphFrame-centric analytics environment.
+
+The current implementation of write requests creates Ignite caches for the provided
+edge and vertex dataframes. It its intended to support bulk write operations.
